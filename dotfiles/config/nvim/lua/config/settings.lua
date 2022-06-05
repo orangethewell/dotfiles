be30ac930a97c75a64cac8d2604ca01d20e580f4
Orangethewell <orangethewell@gmail.com>
2022-06-05 17:00:45 -0300

    NVIM: Add vim.diagnostics better view

--- dotfiles/config/nvim/lua/config/settings.lua
+++ dotfiles/config/nvim/lua/config/settings.lua
@@ -51,3 +51,15 @@ vim.cmd([[
 
 -- Status line var
 vim.cmd([[ let extension = expand('%:e') ]])
+vim.cmd([[ 
+" Set updatetime for CursorHold
+" 300ms of no cursor movement to trigger CursorHold
+set updatetime=300
+" Show diagnostic popup on cursor hold
+autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })
+
+" Goto previous/next diagnostic warning/error
+nnoremap <silent> g[ <cmd>lua vim.diagnostic.goto_prev()<CR>
+nnoremap <silent> g] <cmd>lua vim.diagnostic.goto_next()<CR>
+set signcolumn=yes
+]])
